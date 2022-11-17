1e468e64b7f586e7ef87c786cbf9e87f0fe4f37a
Paul Owoicho <paul.owoicho@aun.edu.ng>
2022-11-17 16:48:54 +0000

    remove print statement

--- src/data_classes/conversational_turn.py
+++ src/data_classes/conversational_turn.py
@@ -69,7 +69,7 @@ class ConversationalTurn:
             if ranking:
                 self.ranking = ranking
 
-    def evaluate_turn(self, measure: str='nDCG@3'):
+    def evaluate_turn(self, measure: str = 'nDCG@3'):
         """Evaluates the ranking in this class based on relevance judgements.
 
         Args:
@@ -86,6 +86,5 @@ class ConversationalTurn:
         score = parsed_measure.calc_aggregate(
             self.relevance_judgements, documents
         )
-        print(score)
         
         return score
